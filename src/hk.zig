a71c49fe84c6944302b808988d65058c79225a9f
Asgard <git@asgard.io>
2025-05-05 13:33:55 -0600

    Expose these

--- src/hk.zig
+++ src/hk.zig
@@ -2,11 +2,16 @@ pub const Loader = @import("hk/Loader.zig");
 pub const NamedVariant = @import("hk/NamedVariant.zig");
 pub const Object = @import("hk/object.zig").Object;
 
+pub const Animation = @import("hk/Animation.zig");
+pub const AnimationBinding = @import("hk/AnimationBinding.zig");
+pub const AnimationContainer = @import("hk/AnimationContainer.zig");
 pub const BaseObject = @import("hk/BaseObject.zig");
 pub const Bone = @import("hk/Bone.zig");
 pub const ReferencedObject = @import("hk/ReferencedObject.zig");
 pub const RootLevelContainer = @import("hk/RootLevelContainer.zig");
 pub const Skeleton = @import("hk/Skeleton.zig");
 pub const SkeletonMapper = @import("hk/SkeletonMapper.zig");
+pub const SkeletonMapperData = @import("hk/SkeletonMapperData.zig");
+pub const SplineCompressedAnimation = @import("hk/SplineCompressedAnimation.zig");
 
 pub const vector_types = @import("hk/vector_types.zig");
